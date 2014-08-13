7287a62218ca194601eea96ea5311f0735c878a0
behtash <behtash@gmail.com>
2014-08-14 00:19:12 +1000

    close #19

---
+++ OziBazaar.Database/dbo/IsValidLookupType.sql
@@ -0,0 +1,10 @@
+﻿CREATE FUNCTION dbo.IsValidLookupType(@type nvarchar(50))
+	
+	RETURNs bit AS
+
+	BEGIN
+	IF EXists (select [LookupID] from [dbo].[Lookup]  where [Type]=@type)
+	REturn 1
+	
+	REturn 0
+	END
